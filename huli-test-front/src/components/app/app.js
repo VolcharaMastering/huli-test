81d9879c120e7fda51e84201bd4c2a3dc4e6d4e1
Aleksey Barabanov <forarbaiten@yandex.ru>
2023-04-17 02:19:35 +0200

    frontMarkup/ start. First components

--- huli-test-front/src/components/app/app.js
+++ huli-test-front/src/components/app/app.js
@@ -1,6 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Switch, useLocation, useHistory } from "react-router-dom";
-import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 import { Provider } from 'react-redux';
+import "../../index.css";
+import "./App.css";
 import store from './store';
 
+function App() {
+
+    return (
+        <main className='main'>
+            <Header />
+            <Switch>
+                <Route path="/">
+                    <Main />
+                </Route>
+                <Route path="/charts">
+                    <Charts />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/*">
+                    <NotFound makeMark={makeMark} />
+                </Route>
+            </Switch>
+        </main>
+    )
+}
+
+export default App;
